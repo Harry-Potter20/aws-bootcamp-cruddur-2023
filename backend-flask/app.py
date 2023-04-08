0bf4088d2910a9540ec0c05d43740e49b44ce1c6
Harry-Potter20 <chukwudieke61@gmail.com>
2023-04-08 01:05:08 +0000

    implementing jwt server side verification

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -160,8 +160,11 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-    data = HomeActivities.run()
-    return data, 200
+  print(
+    request.headers.get('Authorization')
+  )
+  data = HomeActivities.run()
+  return data, 200
 
 
 @app.route("/api/activities/notifications", methods=['GET'])
